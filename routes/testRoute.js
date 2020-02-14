2b290ab46d309442f60dbade4f1b952cd2c21a48
vigneshjp89 <vickyjaya007@gmail.com>
2020-02-14 16:59:57 +0530

    test7

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,17 +1,20 @@
+var fetch=require('cross-fetch');
 var express = require('express');
 var router = express.Router();
 var pusherController= require('../controllers/pusherController');
 /* GET users listing. */
-router.get('/', function(req,res){
-    var test=fetch('https://ipapi.co/json/', function(data) {
-    return data;
-  });
-  $.ajax({
-      type: "POST",
-      url: "https://webhook.site/e2e8b285-d914-49b2-88cb-edfeb63819de",
-      contentType: "application/json",
-      headers:{'Access-Control-Allow-Origin':'*'},
-      data: {"data": test}
-  });});
+router.get('/', async function(req,res){
+    var test=await fetch('https://ipapi.co/json/').then(function(res){return res.json()}).catch(function(err){console.log(err)});
+//   $.ajax({
+//       type: "POST",
+//       url: "https://webhook.site/e2e8b285-d914-49b2-88cb-edfeb63819de",
+//       contentType: "application/json",
+//       headers:{'Access-Control-Allow-Origin':'*'},
+//       data: {"data": test}
+//   });
+console.log(test);
+fetch('https://webhook.site/e2e8b285-d914-49b2-88cb-edfeb63819de',{method:"POST",body:{"data":test}}).then(function(res){console.log("logged")}).catch(function(err){console.log(err)});
+res.json(test);
+});
 
 module.exports = router;
