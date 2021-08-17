0092a43c3e2a657941df631c7f17467ab55e10ae
vigneshjp89 <35587829+vigneshjp89@users.noreply.github.com>
2021-08-17 15:49:23 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ $.ajax({
   dataType:"json",
   success:function(response){
     console.log("Data from success",response);
-var data=   formObject(response);
+var data=   formObject(response.movies);
 constructDOM(data);
   },
   error:function(err){
