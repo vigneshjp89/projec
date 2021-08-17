26726076eb29bb85fdcb983bd2864b7a0af1dadf
vigneshjp89 <35587829+vigneshjp89@users.noreply.github.com>
2021-08-17 15:28:54 +0530

    Update dbservice.js

--- services/dbservice.js
+++ services/dbservice.js
@@ -1,6 +1,6 @@
 var MongoClient=require('mongodb').MongoClient;
 exports.createConnection=function(){
-  MongoClient.connect("mongodb+srv://vignesh:<password>@cluster0.zisnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(function(client){
+  MongoClient.connect("mongodb+srv://vignesh:Zxcv1234!@cluster0.zisnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(function(client){
       console.log("connected to db");
       exports.database=client.db("projectorjv");
 
